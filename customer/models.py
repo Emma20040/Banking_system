c5d514693e6317d8d4b71bc80dfb8adb95196e2b
Emma <emmanuelfongong10@gmail.com>
2025-06-04 23:30:57 +0100

    created models for user, account which has function for credit and debit

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    age = models.IntegerField(blank=True, null= True)
+    country= models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null= True)
+    phone = models.IntegerField(blank=True,  null= True)
+    balance = models.DecimalField(max_digits=200, decimal_places=3)
+
+    
+class Account(models.Model):
+    account_num =models.IntegerField(primary_key=True, null= False)
+    account_holder= models.ForeignKey(Customer, on_delete=models.CASCADE , null=True)
+    balance = models.DecimalField(decimal_places=3, max_digits=30, default=0, null= False)
+
+    def __str__(self):
+        return str(self.account_num)
+    
+    def credit(self, amount):
+        try:
+            self.balance = self.balance + int(amount)
+            self.save()
+        except:
+            raise ValueError("you don't have enough money to carry out this transaction")
+        
+    def debit(self, amount):
+        try:
+            if amoumt<=0:
+                raise ValueError("input a valid amount to perform this transaction")
+            if amount >= self.balance:
+                raise ValueError("insufficint funds")
+            
+            self.balance = self.balance - int(amount)
+            self.save()
+        except:
+            raise ValueError("Transaction failed")
