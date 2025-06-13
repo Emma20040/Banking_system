07514cd9a613a3102e9373e0f03e941a22492bf8
Emma <emmanuelfongong10@gmail.com>
2025-06-13 12:23:35 +0100

    added and setup account app

--- bank/settings.py
+++ bank/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'customer',
     'authenticate',
+    'account',
+    
 ]
 
 MIDDLEWARE = [
--- bank/urls.py
+++ bank/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('customer.urls')),
     path('auth/', include('authenticate.urls')),
+    path('account/', include('account.urls')),
 ]
